435b41c2608f1b1028db25e5a85d1dde9fd98cf4
prostestproject <prostestproject@gmail.com>
2023-07-24 16:46:37 +0300

    Pytest add

---
+++ tests/api/test_fixtures.py
@@ -0,0 +1,8 @@
+import pytest
+@pytest.mark.check
+def test_check_name(user):
+    assert user.name == 'Sergii'
+
+@pytest.mark.check
+def test_check_second_name(user):
+    assert user.second_name == 'Butenko'
